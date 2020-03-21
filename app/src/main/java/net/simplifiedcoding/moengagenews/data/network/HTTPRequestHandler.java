290ea22e285b64e3f1f4d67b3e582a288613ae99
Belal <belal@innocirc.com>
2020-03-21 17:09:45 +0530

    News Fetch Completed

--- app/src/main/java/net/simplifiedcoding/moengagenews/data/network/HTTPRequestHandler.java
+++ app/src/main/java/net/simplifiedcoding/moengagenews/data/network/HTTPRequestHandler.java
@@ -1,12 +1,7 @@
 package net.simplifiedcoding.moengagenews.data.network;
 
-import android.util.Log;
-
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
